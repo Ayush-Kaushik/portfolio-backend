85d024333c8ad6363983562d25c01d18badee26c
Ayush Kaushik <anon.programmer305@gmail.com>
2025-09-20 12:43:41 -0400

    Added portfolio insight worker

--- Cloudflare/README.md
+++ Cloudflare/README.md
@@ -1,2 +1,9 @@
-# Tests
-Tests are based on this blog: https://blog.cloudflare.com/workers-vitest-integration/
+# What I am building here
+
+I am building a worker whose purpose is to provide a gateway to gemini ai which has stored my websites information.
+
+so user can query it directly instead of going through website.
+
+For M1, I am focusing on getting the single chat and fine tuning model to produce only content specific to my website.
+
+Also, how to get the website content in a readable format - what is the technology behind ingesting this data for better results.
---
+++ Cloudflare/portfolio-insight-worker/src/index.ts
@@ -0,0 +1,62 @@
+import { GoogleGenAI } from '@google/genai';
+
+export default {
+	async fetch(request: Request, env, ctx): Promise<Response> {
+
+		const GEMINI_API_KEY = env.GEMINI_API_KEY;
+		const MAX_TOTAL_BYTES = 5000; // adjust as safe max payload size
+
+		if (GEMINI_API_KEY === undefined || GEMINI_API_KEY === "") {
+			return new Response("Worker is under maintainence", { status: 500 });
+		}
+
+		try {
+			console.log("Incoming request: ", request.method, request.url, request.headers);
+
+			if (request.method !== "POST") {
+				console.warn("Rejected non-post request");
+				return new Response("Forbidden", { status: 403 });
+			}
+
+			const contentType = request.headers.get("content-type") || "";
+			if (!contentType.includes("application/json")) {
+				return new Response("Content-Type must be application/json", { status: 403 });
+			}
+
+			const contentLengthHeader = request.headers.get("content-length");
+			if (contentLengthHeader && Number(contentLengthHeader) > MAX_TOTAL_BYTES) {
+				return new Response("Forbidden", { status: 403 });
+			}
+
+			let data: Prompt;
+			try {
+				data = await request.json<Prompt>();
+				console.trace(data.prompt);
+			} catch (error) {
+				console.error(error);
+				return new Response("Forbidden", { status: 403 });
+			}
+
+			const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+			const response = await ai.models.generateContent({
+				model: 'gemini-2.0-flash-001',
+				contents: data.prompt
+			});
+
+			return new Response(JSON.stringify(response), { status: 200, headers: { 'Content-Type': 'application/json' } });
+
+		} catch (err: any) {
+			console.error(err);
+			return new Response(`Failed to deliver your message`, { status: 500 });
+		}
+	},
+} satisfies ExportedHandler<Env>;
+
+export interface Prompt {
+	prompt: string;
+}
+
+export interface Env {
+	GEMINI_API_KEY: string;
+}
